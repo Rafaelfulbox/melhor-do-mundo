567e91a234a368f11e89fda0d438768eab240a81
Rafaelfulbox <163150990+Rafaelfulbox@users.noreply.github.com>
2024-03-12 11:26:46 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function mudarimagemfora()
+{
+    document.getElementById("imagem").src ="https://i.pinimg.com/originals/ba/5d/f7/ba5df704a239872432c72602d1e35fb9.jpg";
+}
+
+function mudarimagemdentro()
+{
+    document.getElementById("imagem").src ="https://i.pinimg.com/originals/9d/1f/1c/9d1f1c7e3f7b84f2d92fb05c36f0dfa6.jpg";
+}
